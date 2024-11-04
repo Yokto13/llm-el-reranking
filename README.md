e992ca07abc12f68b1770e3c77cdaa4a63b15242
Dominik Farhan <nikousf@seznam.cz>
2024-11-04 08:01:34 +0100

    improve readability and add a few todo notes

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # LLM based reranker for entity linking
 
-This is a simple proof of concept reranker based on 8b Llama 3.1.
+This is a simple proof of concept reranker based on 8b Llama 3.1. 
+
+The idea is that an LLM could efficiently rerank TOP entities retrieved by [some more efficient process](https://github.com/yokto13/mel). Surprisingly, this does not seem to be the case as Mewsli-9 results show weak performance. I might try a different model in the future. Part of the problem might also come from the prompt, which could benefit from some adjustments.
 
 ## How to run this
 
-WIP
+`main.py` is the entry point to the program. All variables that need to be adjusted are at the top of it.
+Currently, the code expects data from my [other project](https://github.com/yokto13/mel) to work but can be certainly adjusted. To do that change `load_mewsli` and `load_candidates` in `main.py` accordingly.
 
-## Mewsli-9 results
+## Mewsli-9 results (10 candidates)
 
-ar 56.3
-de
-en
-es 64.2
-fa
-ja
-sr
-ta
-tr
+| Lang | R@1  | Upper bound |
+|------|------|-------------|
+| ar   | 56.3 | TBA        |
+| de   | TBA  | TBA        |
+| en   | 65.3 | TBA        |
+| es   | 64.2 | TBA        |
+| fa   | TBA  | TBA        |
+| ja   | TBA  | TBA        |
+| sr   | TBA  | TBA        |
+| ta   | TBA  | TBA        |
+| tr   | TBA  | TBA        |
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from pathlib import Path
 from tqdm import tqdm
-from pprint import pprint
 
 import numpy as np
+from transformers import AutoTokenizer
 
 from reranker import Reranker
 from wiki_retriever import WikiRetriever
@@ -14,34 +13,43 @@ retriever_tokenizer_name = (
     "/lnet/work/home-students-external/farhan/troja/outputs/models/LEALLA-base"
 )
 retriever_tokenized_data_dir = "/lnet/work/home-students-external/farhan/troja/outputs/workdirs/all/damuel_descs_together_tokens"
-mewsli_path = "/lnet/work/home-students-external/farhan/troja/outputs/tokens_mewsli_finetuning/es/mentions_1307770978027216442.npz"
-candidates_path = "es.npz"
+mewsli_path = "/lnet/work/home-students-external/farhan/troja/outputs/tokens_mewsli_finetuning/de/mentions_1215272744215592859.npz"
+candidates_path = "de.npz"
 
 
 _qids_remap = load_qids_remap("damuel_1.1-dev_qid_redirects.json.xz")
 
 
-def main(max_mentions: int) -> None:
-    reranker = Reranker(model_name)
-
-    retriever = WikiRetriever(retriever_tokenizer_name, retriever_tokenized_data_dir)
-
+def load_mewsli(
+    max_mentions: int, tokenizer: AutoTokenizer
+) -> tuple[list[str], list[int]]:
     mewsli = np.load(mewsli_path)
-    queries = [
-        retriever.tokenizer.decode(tokens) for tokens in mewsli["tokens"][:max_mentions]
-    ]
-    mewsli_qids = [
+    queries = [tokenizer.decode(tokens) for tokens in mewsli["tokens"][:max_mentions]]
+    qids = [
         _qids_remap[qid] if qid in _qids_remap else qid
         for qid in mewsli["qids"][:max_mentions]
     ]
+    return queries, qids
 
+
+def load_candidates(
+    max_mentions: int, retriever: WikiRetriever
+) -> tuple[list[list[str]], list[list[int]]]:
     candidates_qids = np.load(candidates_path)["candidate_qids"][:max_mentions]
-
     candidates = [
         [retriever.query(qid) for qid in candidates_qids[i] if qid != -1]
         for i in range(len(candidates_qids))
     ]
+    return candidates, candidates_qids
 
+
+def compute_accuracy(
+    reranker: Reranker,
+    queries: list[str],
+    mewsli_qids: list[int],
+    candidates: list[list[str]],
+    candidates_qids: list[list[int]],
+) -> None:
     correct = 0
     upper_bound = 0
 
@@ -58,6 +66,17 @@ def main(max_mentions: int) -> None:
             print(f"Current accuracy: {correct / (i + 1)}")
     print(f"Accuracy: {correct / len(queries)}")
     print(f"Upper bound: {upper_bound / len(queries)}")
+
+
+def main(max_mentions: int) -> None:
+    reranker = Reranker(model_name)
+    retriever = WikiRetriever(retriever_tokenizer_name, retriever_tokenized_data_dir)
+    tokenizer = retriever.tokenizer
+
+    queries, mewsli_qids = load_mewsli(max_mentions, tokenizer)
+    candidates, candidates_qids = load_candidates(max_mentions, retriever)
+
+    compute_accuracy(reranker, queries, mewsli_qids, candidates, candidates_qids)
 
 
 if __name__ == "__main__":
